3c0f55e8a0b8056988f3a673d431f5280fcb047e
Raul Jimenez <rjbarco@gmail.com>
2023-02-21 10:03:33 +0100

    Node factory build in 'node.js'. Getters and Setters implemented

---
+++ src/node.js
@@ -0,0 +1,35 @@
+// Factory Node
+export const Node = (value,left,right) => {
+    // Attributes
+    let data = value;
+    let lChild = left;
+    let rChild = right;
+
+    // Methods
+    // Getters
+    const getData = () => data;
+    const getLeftChild = () => lChild;
+    const getRightChild = () => rChild;
+
+    // Setters
+    const setData = (val) => {
+        data = val;
+    }
+    const setLeftChild = (node) => {
+        lChild = node;
+    }
+    const setRightChild = (node) => {
+        rChild = node;
+    }
+
+    return {
+        getData,
+        getLeftChild,
+        getRightChild,
+        setData,
+        setLeftChild,
+        setRightChild
+    }
+}
+
+export default Node;
